3df49e19548a02bd1280c26d7abd25fc74eeff92
razakrazu <razakrazu135@gmail.com>
2024-03-12 10:14:54 +0530

    signup all functions

--- lib/controller/authsarvice.dart
+++ lib/controller/authsarvice.dart
@@ -1,3 +1,75 @@
-class AuthSarvices{
-  
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_todo_app/model/sign_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthSarvices {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection('userData');
+
+  Future<UserCredential?> registerUser(SignupModel user) async {
+    UserCredential userData = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: user.email.toString(), password: user.password.toString());
+    if (userData != null) {
+      FirebaseFirestore.instance
+          .collection('userData')
+          .doc(userData.user!.uid)
+          .set({
+        'uid': userData.user!.uid,
+        'email': userData.user!.email,
+        'name': user.name,
+        'createdAt': user.createAt,
+        'status': user.status,
+      });
+      return userData;
+    } else {
+      print('Error');
+    }
+  }
+
+
+
+ Future<DocumentSnapshot?> loginUser(SignupModel user) async{
+
+ DocumentSnapshot? snap;
+SharedPreferences pref = await SharedPreferences.getInstance();
+
+  UserCredential usercreantial = await auth.signInWithEmailAndPassword(email: user.email.toString(), password: user.password.toString());
+
+String ? token = await usercreantial.user!.getIdToken();
+
+
+if(usercreantial != null){
+
+
+   snap =  await userCollection.doc(usercreantial.user!.uid).get();
+await pref.setString('token',token!);
+await pref.setString('name', snap['name']);
+await pref.setString('email', snap['email']);
+return snap;
 }
+
+ } 
+
+Future<void>logOut()async{
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  await pref.clear();
+ await auth.signOut();
+}
+
+
+
+Future<bool>isloggedin()async {
+  SharedPreferences pref  = await SharedPreferences.getInstance();
+  String? token = await pref.getString('token');
+  if(token == null ){
+    return false;
+  }else{
+    return true;
+  }
+}
+}
+
+
--- lib/model/sign_model.dart
+++ lib/model/sign_model.dart
@@ -1,27 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SignupModel{
-  final String  email;
-  final String name;
-  final String password;
-  final String uid;
-  final DateTime createAt;
-  final int status; 
-  SignupModel({required this.name, required this.email, required this.password,required this.createAt,required this.status, required this.uid, });
+class SignupModel {
+  final String? email;
+  final String? name;
+  final String? password;
+  final String? uid;
+  final DateTime? createAt;
+  final int? status;
+  SignupModel({
+     this.name,
+     this.email,
+     this.password,
+     this.createAt,
+     this.status,
+     this.uid,
+  });
 
-  factory SignupModel.fromJson(DocumentSnapshot data){
-    return SignupModel(name: data['name'], email: data['email'], password: data['password'], createAt: data['createAt'], status: data['status'], uid: data['uid']);
+  factory SignupModel.fromJson(DocumentSnapshot data) {
+    return SignupModel(
+        name: data['name'],
+        email: data['email'],
+        password: data['password'],
+        createAt: data['createAt'],
+        status: data['status'],
+        uid: data['uid']);
   }
-  Map<String,dynamic>toJson(){
+  Map<String, dynamic> toJson() {
     return {
-
-      'uid':uid,
-      'email':email,
-      'password':password,
-      'name':name,
-      'status':status,
-      'createAt':createAt,
-
+      'uid': uid,
+      'email': email,
+      'password': password,
+      'name': name,
+      'status': status,
+      'createAt': createAt,
     };
   }
 }
